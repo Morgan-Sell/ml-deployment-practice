3b42055d32437e7946f15d1d3ac433780637acb5
Morgan-Sell <morganpsell@gmail.com>
2021-02-25 22:50:20 -0800

    finished first draft of pipeline.py for Third-Party-Pipeline

--- Assignments/Section-04/Third-Party-Pipeline/Assignment/pipeline.py
+++ Assignments/Section-04/Third-Party-Pipeline/Assignment/pipeline.py
@@ -9,4 +9,28 @@ import config
 titanic_pipe = Pipeline(
     # complete with the list of steps from the preprocessors file
     # and the list of variables from the config
-    )
+    [
+        ('identify_missing_values',
+            pp.MissingIndicator(variables=config.ALL_FEATURES)),
+
+        ('categorical_imputer',
+            pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS)),
+        
+        ('numerical_imputer',
+            pp.NumericalImputer(variables=config.NUMERICAL_VARS)),
+        
+        ('extract_first_letter',
+            pp.ExtractFirstLetter(variables=config.CABIN)),
+        
+        ('encode_rare_categorical_labels',
+            pp.RareLabelCategoricalEncoder(variables=config.CATEGORICAL_VARS)),
+        
+        ('one_hot_encoder_categorical',
+            pp.CategoricalEncoder(varialbes=config.CATEGORICAL_VARS)),
+        
+        ('scaler', StandardScaler()),
+
+        ('logistic_regression', LogisticRegression(C=0.0005, random_state=0))
+
+    ]
+)
